7644245507028d230ca06bd130d69ce16bd0d5c2
raulatila <raul.atila10@gmail.com>
2026-04-18 20:51:07 -0300

    style: limpa raiz do repositório e move README para o topo

--- config.example.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Copie este arquivo como config.php e configure seu .env
-define('APP_NAME',    'EduGestor');
-define('APP_VERSION', '3.0.0');
-define('DB_PATH',     __DIR__ . '/database/escola.db');
-// ... resto das definições (Roles, etc)
